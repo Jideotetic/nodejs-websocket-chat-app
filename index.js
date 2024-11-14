c52d9f53c72448cf399f895214556d6196725db8
Jideotetic <jideotetic@gmail.com>
2024-11-14 08:31:43 +0100

    Persist message on client disconnection

--- index.js
+++ index.js
@@ -2,17 +2,58 @@ import "dotenv/config";
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+
+await db.exec(`
+      CREATE TABLE IF NOT EXISTS messages (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          client_offset TEXT UNIQUE,
+          content TEXT
+      );
+    `);
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+io.on("connection", async (socket) => {
+  socket.on("chat message", async (msg, callback) => {
+    callback({
+      status: "ok",
+    });
+
+    let result;
+    try {
+      result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+    } catch (e) {
+      return;
+    }
+    io.emit("chat message", msg, result.lastID);
   });
+
+  if (!socket.recovered) {
+    try {
+      await db.each(
+        "SELECT id, content FROM messages WHERE id > ?",
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
+        }
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  }
 });
 
 server.listen(process.env.PORT ?? 3001, () => {
